4f659e35897eafc3b5e0793edce0a6bfa31384e0
Carlos Alvarez <caralva527@gmail.com>
2025-04-29 14:04:54 -0600

    primer lanzamiento de este paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="releaseRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Carlos Alvarez",                         # Tu nombre
+    author_email="alvarez527@hotmail.com",                 # Tu correo electrónico
+    url="https://github.com/Alvarez527/releaseRepo",     # URL del proyecto
+)
